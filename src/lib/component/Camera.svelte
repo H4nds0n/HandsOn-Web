600366c3e0c08a21f5e3f1b7d1be628efe2b2ae5
Fellograce <trinh.christian31@gmail.com>
2023-11-15 15:03:50 +0100

    css changes

--- src/lib/component/Camera.svelte
+++ src/lib/component/Camera.svelte
@@ -3,9 +3,43 @@
     import * as camera from "$lib/scripts/Camera.js"
     import {textareaValue} from "$lib/scripts/stores.js";
 
+    let video;
+    let canvas;
+    let ctx;
+    let letter = {letter: "", conf: 0.0}
     onMount(() => {
         // This code will run after the component is mounted and the DOM is ready
-        camera.startCamera();
+        //camera.startCamera();
+
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(stream => {
+                video.srcObject = stream;
+            })
+            .catch(error => console.error('Error accessing webcam:', error));
+
+        // Set up canvas
+        ctx = canvas.getContext('2d');
+        const captureInterval = 200; // milliseconds
+
+        // Capture images every few milliseconds
+        setInterval(async () => {
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+            const imageData = canvas.toDataURL('image/jpeg');
+            const data = {
+                "img": imageData
+            }
+            letter = await fetch('http://localhost:5000/streaming', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            }).then(data => data.json());
+            console.log(letter)
+
+            // You can now send 'imageData' to your server or perform any other actions
+        }, captureInterval);
     })
 </script>
 
@@ -22,12 +56,17 @@
     #videoElement {
         transform: scaleX(-1);
     }
+
+    canvas
+    {
+        display: none;
+    }
 </style>
 
 
 <div class="left-comp">
-    <video autoplay="true" id="videoElement" class="rounded-lg">
+    <video autoplay="true" bind:this={video} class="rounded-lg">
         <track kind="captions">
     </video>
-    <canvas id="frame"></canvas>
+    <canvas bind:this={canvas}></canvas>
 </div>
