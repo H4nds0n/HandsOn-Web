aaaabeb88eb8e5c670dc2c6a6d1c1ec7f4607ddc
Samuel Mittermair <samuel.mittermair@htl-wels.at>
2024-01-17 16:46:16 +0100

    Quiz: Word recognition finished

--- src/lib/scripts/stores.js
+++ src/lib/scripts/stores.js
@@ -6,5 +6,14 @@ export const textareaValue = writable('');
 
 export let quizActive = writable(false);
 
+export let letterQuestion = writable("");
+export let letterAnswer = writable("");
+
+export let wordQuestion = writable("");
+export let wordAnswer = writable("");
+
+export let answerCorrect = writable(false)
+export let leveltwo = writable(false)
+
 export const hideHistory = writable(true);
 export let textHistory = writable([]);
